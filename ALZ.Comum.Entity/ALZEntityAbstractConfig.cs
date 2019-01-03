51ec1588de77ad54d59d73a53c4f543389d615ea
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2019-01-03 10:52:04 -0200

    .

---
+++ ALZ.Comum.Entity/ALZEntityAbstractConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALZ.Comum.Entity
+{
+    public abstract class ALZEntityAbstractConfig<TEntidade> : EntityTypeConfiguration<TEntidade>
+        where TEntidade : class
+    {
+        public ALZEntityAbstractConfig()
+        {
+            ConfigurarNomeTabela();
+            ConfigurarCamposTabela();
+            ConfigurarChavePrimaria();
+            ConfigurarChavesEstrangeiras();
+        }
+
+        protected abstract void ConfigurarChavesEstrangeiras();
+        protected abstract void ConfigurarChavePrimaria();
+        protected abstract void ConfigurarCamposTabela();
+        protected abstract void ConfigurarNomeTabela();
+    }
+}
---
+++ ALZ.ConsultaProcessosTjmg.AcessoDados.Entity/Context/ConsultaProcessosDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ALZ.ConsultaProcessosTjmg.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALZ.ConsultaProcessosTjmg.AcessoDados.Entity.Context
+{
+    public class ConsultaProcessosDbContext : DbContext
+    {
+        public DbSet<Processo> Processos { get; set; }
+    }
+}
